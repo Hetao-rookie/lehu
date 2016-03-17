e1645c2e9aa4eabacf4a28237ce6d2f1361fe45c
古月 <guyue.me@gmail.com>
2016-03-17 11:00:07 +0800

    定义权限表

--- database/migrations/2016_03_16_160108_create_permissions_table.php
+++ database/migrations/2016_03_16_160108_create_permissions_table.php
@@ -7,24 +7,23 @@ class CreatePermissionsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
-      Schema::create('permissions', function (Blueprint $table) {
-          $table->integer('id');
-          $table->string('ident');
-          $table->string('name');
-          $table->string('attachment');
-          $table->primary('id');
-      });
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('ident')->unique();
+            $table->string('name');
+            $table->string('resource');
+            $table->string('resource_field');
+            $table->string('resource_id');
+            $table->string('source');
+            $table->primary('id');
+        });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
